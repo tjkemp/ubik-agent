6f010bef9dd1ef197478359f31e77976ae0d3233
tjkemp <tero.kemppi@gmail.com>
2020-05-25 21:13:12 +0300

    restructure banana executable for clarity

--- banana.py
+++ banana.py
@@ -8,9 +8,9 @@ import numpy as np
 from agent.interaction import UnityInteraction
 from agent.helper import get_model_dir, create_model_dir, save_scores
 from agent import DQNAgent
+from agent.agent import RandomAgent
 
 ENV_PATH = './environments/Banana_Linux/Banana.x86_64'
-SAVEFILE = 'checkpoint.pth'
 
 MODEL_PARAMS = {
     'learning_rate': 5e-4,
@@ -23,17 +23,15 @@ MODEL_PARAMS = {
 }
 
 TRAINING_PARAMS = {
-    'num_episodes': 800,
+    'num_episodes': 500,
     'max_time_steps': 300,
     'target_score': 13.0,
 }
 
-
-def main(mode, modelname):
+def train(modelname):
 
     # create environment
-    no_graphics = True if mode == 'train' else False
-    env = UnityEnvironment(file_name=ENV_PATH, no_graphics=no_graphics)
+    env = UnityEnvironment(file_name=ENV_PATH, no_graphics=True)
 
     # create an agent
     state_size, action_size, num_agents = UnityInteraction.stats(env)
@@ -42,28 +40,80 @@ def main(mode, modelname):
     # create train or run loop
     sim = UnityInteraction(agent, env)
 
-    if mode == 'train':
+    if modelname is not None:
         create_model_dir(modelname)
-        scores = sim.train(**TRAINING_PARAMS)
-        modelfile = os.path.join(get_model_dir(modelname), SAVEFILE)
+
+    scores = sim.train(**TRAINING_PARAMS)
+    modelfile = os.path.join(get_model_dir(modelname))
+
+    if modelname is not None:
         agent.save(modelfile)
         save_scores(modelname, scores)
 
-    elif mode == 'run':
-        modelfile = os.path.join(get_model_dir(modelname), SAVEFILE)
-        agent.load(modelfile)
-        sim.run()
-
     env.close()
 
+def run(modelname):
 
-if __name__ == "__main__":
+    # create environment
+    env = UnityEnvironment(file_name=ENV_PATH, no_graphics=False)
+
+    # create an agent
+    state_size, action_size, num_agents = UnityInteraction.stats(env)
+    agent = DQNAgent(state_size, action_size, num_agents, **MODEL_PARAMS)
+    modelfile = os.path.join(get_model_dir(modelname))
+    agent.load(modelfile)
+
+    # create train or run loop
+    sim = UnityInteraction(agent, env)
+    sim.run()
+    env.close()
+
+def random_run():
+
+    # create environment
+    env = UnityEnvironment(file_name=ENV_PATH, no_graphics=False)
+
+    # create an agent
+    state_size, action_size, num_agents = UnityInteraction.stats(env)
+    agent = RandomAgent(
+        state_size, action_size, action_type='discrete', num_agents=num_agents)
+
+    # create train or run loop
+    sim = UnityInteraction(agent, env)
+    sim.run()
+    env.close()
+
+def main():
 
     parser = argparse.ArgumentParser(
         description='Runs or trains an agent in Unity BananaCollector environment.')
-    parser.add_argument(
-        'mode', choices=['train', 'run'], help="either 'train' or 'run'")
-    parser.add_argument(
-        'modelname', help="name of the agent's model in models directory")
+
+    subparsers = parser.add_subparsers(
+        title='subcommands', dest='subcommand', help='additional help   ')
+
+    parser_train = subparsers.add_parser('train', help='train an agent')
+    parser_train.add_argument(
+        'modelname', nargs='?', help="directory name in models where to save the agent model")
+
+    parser_run = subparsers.add_parser(
+        'run', help='run environment with trained agent')
+    parser_run.add_argument(
+        'modelname', help="directory name in models from where to load the agent model")
+
+    parser_random = subparsers.add_parser(
+        'random', help='run environment with randomly acting agent')
+    parser_random.set_defaults(modelname=None)
+
     args = parser.parse_args()
-    main(args.mode, args.modelname)
+
+    if args.subcommand == 'train':
+        train(args.modelname)
+    elif args.subcommand == 'run':
+        run(args.modelname)
+    elif args.subcommand == 'random':
+        random_run()
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
