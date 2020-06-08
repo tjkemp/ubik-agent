7dbf23bbad3a14801c828625bfaca96b984c87b5
tjkemp <tero.kemppi@gmail.com>
2020-06-08 13:45:49 +0300

    move agents into their own folder

--- ubikagent/__init__.py
+++ ubikagent/__init__.py
@@ -1,6 +1,2 @@
-from .dqn import DQNAgent
-from .ddpg import DDPGAgent
-from .sarsa import SarsaAgent
-from .agent import Agent, RandomGymAgent, RandomAgent
 from .interaction import Interaction, UnityInteraction, GymInteraction
 from .buffer import ReplayBuffer
---
+++ ubikagent/agent/__init__.py
@@ -0,0 +1,4 @@
+from .agent import Agent, RandomGymAgent, RandomAgent
+from .dqn import DQNAgent
+from .ddpg import DDPGAgent
+from .sarsa import SarsaAgent
